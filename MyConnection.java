c160833cd9020c7eaf2b19b5beb2d251a4a7ad61
Md.Akter Hossain <48676203+mdakterh627@users.noreply.github.com>
2019-06-10 08:38:53 +0600

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,23 @@
+
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class MyConnection {
+    public static Connection getConnection() {
+        Connection con =null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Akter","root","");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+     return con;   
+    }
+}
